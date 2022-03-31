acc1af67f852b114e263c2d54ec73c4117255e6e
iffanadn <iffan1951@gmail.com>
2022-03-31 23:38:27 +0700

    navbar terbaru

--- app/Views/layout/v_nav.php
+++ app/Views/layout/v_nav.php
@@ -1,38 +1,13 @@
-<div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Hidden Gem</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item ms-3">
-                        <a class="nav-link active" href="<?= base_url('home') ?>" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item ms-3">
-                        <a class="nav-link" href="<?= base_url('feed') ?>">Feed</a>
-                    </li>
-                    <li class="nav-item ms-3">
-                        <a class="nav-link" href="<?= base_url('explore') ?>">Explore</a>
-                    </li>
-                    <li class="nav-item ms-3">
-                        <a class="nav-link" href="<?= base_url('blog') ?>">Blog</a>
-                    </li>
-                    <li class="nav-item ms-3">
-                        <a class="nav-link" href="<?= base_url('profile') ?>">Profile</a>
-                    </li>
-
-                    <li class="nav-item ms-3">
-                        <a class="nav-link" href="<?= base_url('login/logout') ?>">Logout</a>
-                    </li>
-                </ul>
-
-            </div>
+<div class="head-container">
+    <div class="head-row">
+        <div class="nav-logo">HIDDEN <span>GEM</span></div>
+        <div class="nav-menu">
+            <ul>
+                <li>Feed</li>
+                <li>Explore</li>
+                <li>Blog</li>
+                <li>Profile</li>
+            </ul>
         </div>
-    </nav>
+    </div>
 </div>
