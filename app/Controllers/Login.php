3487e3f0618817c822fc4264334e845c22c7e290
muhammadzidane633@gmail.com <muhammadzidane633@gmail.com>
2022-04-04 05:12:17 +0700

    Nav Update

---
+++ app/Controllers/Login.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use App\Models\LoginModel;
+
+class Login extends BaseController
+{
+    public function __construct()
+    {
+        helper('form');
+        $this->LoginModel = new LoginModel();
+    }
+
+    public function index()
+    {
+        echo view('v_login');
+    }
+
+    public function cek_login()
+    {
+        if ($this->validate([
+            'email' => [
+                'label'  => 'e-mail',
+                'rules'  => 'required',
+                'errors' => [
+                    'required' => 'Email Wajib diisi!!',
+                ],
+            ],
+            'password' => [
+                'label'  => 'password',
+                'rules'  => 'required',
+                'errors' => [
+                    'required' => 'Password Wajib diisi!!',
+                ],
+            ],
+        ])) { //jika valid
+            $email = $this->request->getPost('email');
+            $password = base64_encode($this->request->getPost('password'));
+            $cek = $this->LoginModel->cek_login($email, $password);
+            if ($cek) {
+                session()->set('log', true);
+                session()->set('id_user', $cek['id_user']);
+                session()->set('nama_user', $cek['nama_user']);
+                session()->set('email', $cek['email']);
+                return redirect()->to(base_url('home'));
+            } else {
+                session()->setFlashData('pesan', 'Login Gagal, Username atau Password Salah!');
+                return redirect()->to(base_url('login'));
+            }
+        } else {
+            session()->setFlashData('errors', \Config\Services::validation()->getErrors());
+            return redirect()->to(base_url('/login'));
+        }
+    }
+
+    public function logout()
+    {
+        session()->remove('log');
+        session()->remove('id_user');
+        session()->remove('nama_user');
+        session()->remove('email');
+
+        session()->setFlashData('pesan', 'Anda telah Logout');
+        return redirect()->to(base_url('login'));
+    }
+};
--- app/Views/layout/v_nav.php
+++ app/Views/layout/v_nav.php
@@ -1,12 +1,15 @@
 <div class="head-container">
     <div class="head-row">
-        <div class="nav-logo">HIDDEN <span>GEM</span></div>
+        <div class="nav-logo">
+            <a href="home" style="text-decoration: none; color:#fcfcd4">HIDDEN <span>GEM</span></a>
+        </div>
         <div class="nav-menu">
             <ul>
-                <li><a href="#">Feed</a></li>
-                <li><a href="#">Explore</a></li>
-                <li><a href="#">Blog</a></li>
-                <li class="login"><a href="#">Profile</a></li>
+                <li><a href="<?= base_url('Feed') ?>">Feed</a></li>
+                <li><a href="<?= base_url('explore') ?>">Explore</a></li>
+                <li><a href="<?= base_url('blog') ?>">Blog</a></li>
+                <li class="login"><a href="<?= base_url('Profile') ?>">Profile</a></li>
+                <li><a href="<?= base_url('login/logout') ?>">Logout</a></li>
             </ul>
         </div>
     </div>
